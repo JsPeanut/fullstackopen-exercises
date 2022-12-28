ce7c7a10467d9d1f4993b8d69d922417eb0e7d1f
JsPeanut <110200246+JsPeanut@users.noreply.github.com>
2022-12-28 13:28:34 -0300

    Add files via upload

---
+++ part1/courseinfo/step2/App.js
@@ -0,0 +1,20 @@
+import Total from "./Total"
+import Header from "./Header"
+import Content from "./Content"
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+  )
+}
+
+export default App
